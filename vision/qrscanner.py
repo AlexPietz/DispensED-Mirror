d32bc92daf7fa02b15e8b02950053223709be7bd
Jasper <jasper.prive@live.nl>
2018-02-02 11:40:13 +0000

    added comments

--- vision/qrscanner.py
+++ vision/qrscanner.py
@@ -8,11 +8,15 @@ def detect_qr(img):
     :param img: CV image to scan
     :return: True if QR code is found, otherwise False
     """
+    # Find edges
     edges = cv2.Canny(img, 100, 200)
     (_, contours, hierarchy) = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     mark = 0
 
+    # Find nested contours, if we find 6 of the (1 parent with 5 children), we assume this is one of the corners
+    # of a QR code. Additional robustness may be added by looking at surface enclosed by the contours and
+    # center of masses.
     for i in range(0, len(contours)):
         k = i
         c = 0
