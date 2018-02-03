5cca2e72e1bc3038e22c119db7da99213ead8997
Jasper <jasper.prive@live.nl>
2018-02-03 12:03:32 +0000

    fixed incorrect lower boundary

--- vision/linedetect.py
+++ vision/linedetect.py
@@ -9,20 +9,21 @@ def detect_line(img, hue):
     :param hue: The hue value to Threshold on
     :return: A binary (thresholded) image
     """
-    hue_lower = hue - 15
-    hue_upper = hue + 15
+    hue_lower = hue - 10
+    hue_upper = hue + 10
+    print(str(hue_upper) + str(hue_lower))
 
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     if hue_lower < 0:
-        hue_lower_1 = 255 - hue_lower
-        thresholded_1 = cv2.inRange(hsv_img, np.array([hue_lower_1, 100, 100]), np.array([255, 100, 100]))
-        thresholded_2 = cv2.inRange(hsv_img, np.array([0, 100, 100]), np.array([hue_upper, 100, 100]))
+        hue_lower_1 = 179 + hue_lower
+        thresholded_1 = cv2.inRange(hsv_img, np.array([hue_lower_1, 100, 100]), np.array([179, 255, 255]))
+        thresholded_2 = cv2.inRange(hsv_img, np.array([0, 100, 100]), np.array([hue_upper, 255, 255]))
         thresholded = cv2.bitwise_or(thresholded_1, thresholded_2)
-    elif hue_upper > 255:
-        hue_upper_1 = hue_upper - 255
-        thresholded_1 = cv2.inRange(hsv_img, np.array([hue_lower, 100, 100]), np.array([255, 100, 100]))
-        thresholded_2 = cv2.inRange(hsv_img, np.array([0, 100, 100]), np.array([hue_upper_1, 100, 100]))
+    elif hue_upper > 179:
+        hue_upper_1 = hue_upper - 179
+        thresholded_1 = cv2.inRange(hsv_img, np.array([hue_lower, 100, 100]), np.array([179, 255, 255]))
+        thresholded_2 = cv2.inRange(hsv_img, np.array([0, 100, 100]), np.array([hue_upper_1, 255, 255]))
         thresholded = cv2.bitwise_or(thresholded_1, thresholded_2)
     else:
         thresholded = cv2.inRange(hsv_img, np.array([hue_lower, 100, 100]), np.array([hue_upper, 255, 255]))
