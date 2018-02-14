1295f015754165619d3cd99c855f55ed2e6483ff
Alexander Pietz <s1529373@sms.ed.ac.uk>
2018-02-14 17:55:23 +0000

    Fix Database Structure (M2M)

--- dispensed-web/avenv.sh
+++ dispensed-web/avenv.sh
@@ -1,4 +1,8 @@
 source venv/bin/activate
+
 export FLASK_APP=dispensed.py
+
+export SQLALCHEMY_DATABASE_UR=I/afs/inf.ed.ac.uk/user/s15/s1529373/sdp/dispensed-web/app.db
+export SQLALCHEMY_TRACK_MODIFICATIONS=False 
 flask run
 
