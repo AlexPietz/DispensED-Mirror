14e72f71a713ac1358f1928394a29002003b9c97
Alexander Pietz <s1529373@sms.ed.ac.uk>
2018-02-15 16:00:34 +0000

    Add bootstrap theme

--- dispensed-web/app/__init__.py
+++ dispensed-web/app/__init__.py
@@ -3,13 +3,14 @@ from config import Config_Disp
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config_Disp)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-
+bootstrap = Bootstrap(app)
 
 from app import routes, models
 
