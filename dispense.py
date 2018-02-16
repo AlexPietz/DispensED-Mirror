32e864d1d02dea17d295e0943ebbee0ab137e9da
Glen Merry <s1531807@texan.inf.ed.ac.uk>
2018-02-16 12:46:39 +0000

    Cleaned up dispensing script

--- dispense.py
+++ dispense.py
@@ -4,60 +4,57 @@ import ev3dev.ev3 as ev3
 import time
 
 cl = ev3.ColorSensor(ev3.INPUT_2)
+cl.mode='COL-REFLECT'
 motor=ev3.Motor('outA')
 
-cl.mode='COL-REFLECT'
-
-numberPills = int(sys.argv[1])
-count_dispensed = 0
-forward = True
-direction_count = 0
-dispensing = False
-initial = cl.value()
-less = False
-stopfor = 0
+numberPills = int(sys.argv[1]) # number of pills to be dispensed
+count_dispensed = 0 # number of pills that have been dispensed so far
+forward = True # direction of motor
+direction_count = 0 # number of iterations since direction change
+dispensing = False # whether dispenser is curently in the process of dispensing
+initial = cl.value() # initial value of colour sensor
+less = False # represents whether cl.value() decreases (or increases) at start of dispensing
+stopfor = 0 # if cl value increases, then we stop checking colour sensor for 10 iterations
 
 motor.run_timed(speed_sp=-450, time_sp=120)
 
 while (count_dispensed < numberPills):
-#    print(cl.value())
+    # reverse direction every 200 iterations
     if (direction_count > 200):
         if (forward):
            motor.run_timed(speed_sp=450, time_sp=120)
-           forward = False
-           direction_count = 0
         else:
            motor.run_timed(speed_sp=-450, time_sp=120)
-           forward = True
-           direction_count = 0
-    
+        forward = not forward
+        direction_count = 0
+
     if (not dispensing and stopfor == 0):
+        # if sensor value changes by +-2 then we have started pill dispensing
+        # sensor behaviour after initial change is dependent on pill position
+        # so cases of value increase and decrease must be seperated
         if (cl.value() <= (initial-2)):
-           #print('downdowndowndowndowndowndowndown')
            dispensing = True
            less = True
-        #   count_dispensed = count_dispensed + 1
-         #  print(str(count_dispensed) + ' pills dispensed')
         elif (cl.value() >= (initial+2)):
-           #print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
            less = False
            dispensing = True
-        #   count_dispensed = count_dispensed + 1
-         #  print(count_dispensed)
-       #    initial = cl.value()
 
     elif (stopfor == 0):
         if (less and cl.value() > (initial-2)):
+            # if sensor value originally decreased, and value has now increased
+            # then dispense is complete
             dispensing = False
             count_dispensed = count_dispensed + 1
             print(str(count_dispensed) + ' pills dispensed')
         elif (not less and cl.value() <= (initial+1) and cl.value() >= (initial-1)):
+            # if sensor value originally increased, it may decrease below normal
+            # before returning to normal, so stop monitoring sensor value for 10 iterations
             dispensing = False
             count_dispensed = count_dispensed + 1
             print(str(count_dispensed) + ' pills dispensed')
-            stopfor = 10                
-           
+            stopfor = 10
+
     direction_count = direction_count + 1
     if (stopfor != 0):
         stopfor = stopfor - 1
-    time.sleep(0.0005)
+    time.sleep(0.0005) # increase length of iteration
