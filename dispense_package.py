ab52fb8de90d9c9d5d873462b77647b470fbb59f
Tizzy M <tizzymac@icloud.com>
2018-02-27 14:44:59 +0000

    Add timeout

--- dispense_package.py
+++ dispense_package.py
@@ -10,10 +10,14 @@ colour = sys.argv[1]
 colour_codes = {"black":1, "blue":2, "green":3, "yellow":4, "red":5, "white":6, "brown":7}
 stop_times = {1:0.1, 2:0.1, 3:0.04, 4:0.1, 5:0.1, 6:0.1, 7:0.2}
 skip = False
+timed_out = False
 try:
     colour_code = colour_codes[sys.argv[1]]
 except:
     print('Invalid colour')
+
+start_time = time.time()
+print(start_time)
 
 if (cl.value() == colour_code):
     skip = True
@@ -21,13 +25,19 @@ if (cl.value() == colour_code):
 if not skip:
     motor.run_forever(speed_sp=150)
 
-    while (cl.value() != colour_code):
+    while ((cl.value() != colour_code) and (not timed_out)):
+        if ((time.time() - start_time) > 6.0):
+            timed_out = True
+            motor.stop()
         pass
 
     time.sleep(stop_times[colour_code])
     motor.stop()
 
-print('found ' + colour)
+if (timed_out):
+    print('Timed out')
+else:
+    print('found ' + colour)
 
 cl.mode='COL-REFLECT'
 
@@ -38,3 +48,4 @@ initial = cl.value()
 while True:
     if cl.value() <= (initial-2) or cl.value() >= (initial+2):
         print('Package taken')
+
