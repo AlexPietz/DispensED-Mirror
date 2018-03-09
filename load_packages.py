7b7065cc329e511c5c206c2463280b3b01359f0a
Tizzy M <32516974+tizzymac@users.noreply.github.com>
2018-03-09 14:48:43 +0000

    Script for loading packages

---
+++ load_packages.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import sys
+import ev3dev.ev3 as ev3
+import time
+
+cl = ev3.ColorSensor(ev3.INPUT_2)
+cl.mode='COL-COLOR'
+motor = ev3.Motor('outD')
+colour = sys.argv[1]
+colour_codes = {"black":1, "blue":2, "green":3, "yellow":7, "red":5, "white":6, "brown":7}
+package_colour_codes = {"blue":2, "green":3, "red":5, "white":6}
+stop_times = {1:0.1, 2:0.12, 3:0.08, 4:0.1, 5:0.1, 6:0.1, 7:0.2}
+skip = False
+timed_out = False
+colour_code = 7
+num_packages_to_load = sys.argv[1]
+
+start_time = time.time()
+
+for n in range(0, int(num_packages_to_load)):
+    print ('Package to load: ' + str(n+1))
+
+    if (cl.value() == colour_code):
+        skip = True
+
+    if not skip:
+        motor.run_forever(speed_sp=150)
+
+        while ((cl.value() != colour_code) and (not timed_out)):
+            if ((time.time() - start_time) > (10.0 * int(num_packages_to_load))):
+                timed_out = True
+                motor.stop()
+            pass
+
+        time.sleep(stop_times[colour_code])
+        motor.stop()
+
+    if (timed_out):
+        print('Timed out. No free slots found.')
+        sys.exit(0)
+    else:
+        print('found free slot')
+
+    cl.mode='COL-REFLECT'
+
+    time.sleep(1)
+
+    initial = cl.value()
+
+    while True:
+        if cl.value() <= (initial-2) or cl.value() >= (initial+2):
+            print('Hand detected')
+            break
+
+    # check if color has changed
+    cl.mode='COL-COLOR'
+    # check hand is removed and package has been placed in
+    print('Waiting for package to be placed in')
+    while True:
+        #if (cl.value in package_colour_codes):
+        if (cl.value != colour_code):
+            print('Package placed in')
+            time.sleep(2)
+            break
+
