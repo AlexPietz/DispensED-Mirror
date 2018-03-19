7fa76d2853e9392468d38c710bbc4271edd3f01f
Glen Merry <gmerry01@gmail.com>
2018-03-19 20:04:06 +0000

    Improvements to dispensing scripts (individual and package)

--- dispensing/dispense_combined_tray.py
+++ dispensing/dispense_combined_tray.py
@@ -4,43 +4,38 @@ import ev3dev.ev3 as ev3
 import time
 
 cl_A = ev3.ColorSensor(ev3.INPUT_1)
-cl_A.mode='COL-REFLECT'
+cl_A.mode = 'COL-REFLECT'
 cl_B = ev3.ColorSensor(ev3.INPUT_2)
-cl_B.mode='COL-REFLECT'
-motor_A=ev3.MediumMotor('outC')
-motor_B=ev3.MediumMotor('outB')
+cl_B.mode = 'COL-REFLECT'
+motor_A = ev3.MediumMotor('outC')
+motor_B = ev3.MediumMotor('outB')
 
-number_pills_1 = int(sys.argv[1]) # number of pills to be dispensed by dispenser A
-number_pills_2 = int(sys.argv[2]) # number of pills to be dispensed by dispenser B
+number_pills = (int(sys.argv[1]), int(sys.argv[2]))
 
-i = 0
-while i < 2:
-    if i == 0:
-        motor = motor_A
-        number_pills = number_pills_1
-    else:
-        motor = motor_B
-        number_pills = number_pills_2
 
-    forward = True # direction of motor
-    direction_count = 0 # number of iterations since direction change
-    dispensing = False # whether dispenser is curently in the process of dispensing
-    initial = cl_A.value() # initial value of colour sensor
-    less = False # represents whether cl.value() decreases (or increases) at start of dispensing
-    stopfor = 0 # if cl value increases, then we stop checking colour sensor for 10 iterations
+def dispense(motor, no_to_dispense):
+    if no_to_dispense == 0:
+        return
+
+    forward = True  # direction of motor
+    direction_count = 0  # number of iterations since direction change
+    dispensing = False  # whether dispenser is curently in the process of dispensing
+    initial = cl_A.value()  # initial value of colour sensor
+    less = False  # represents whether cl.value() decreases (or increases) at start of dispensing
+    stopfor = 0  # if cl value increases, then we stop checking colour sensor for 10 iterations
     dispense_attempts = 0
-    count_dispensed = 0 # number of pills that have been dispensed so far
+    count_dispensed = 0  # number of pills that have been dispensed so far
 
     motor.run_timed(speed_sp=-250, time_sp=190)
-    while (count_dispensed < number_pills):
-        if (dispense_attempts >= 10):
+    while count_dispensed < no_to_dispense:
+        if dispense_attempts >= 10:
             print('Error: dispenser time-out! - pill not dispensed')
             motor.stop()
             break
 
         # reverse direction every 200 iterations
-        if (direction_count > 200):
-            if (forward):
+        if direction_count > 200:
+            if forward:
                 motor.run_timed(speed_sp=250, time_sp=190)
             else:
                 motor.run_timed(speed_sp=-250, time_sp=190)
@@ -48,19 +43,19 @@ while i < 2:
             forward = not forward
             direction_count = 0
 
-        if (not dispensing and stopfor == 0):
+        if not dispensing and stopfor == 0:
             # if sensor value changes by +-2 then we have started pill dispensing
             # sensor behaviour after initial change is dependent on pill position
             # so cases of value increase and decrease must be seperated
-            if (cl_A.value() <= (initial-2)):
-               dispensing = True
-               less = True
-            elif (cl_A.value() >= (initial+2)):
-               less = False
-               dispensing = True
+            if cl_A.value() <= (initial - 2):
+                dispensing = True
+                less = True
+            elif cl_A.value() >= (initial + 2):
+                less = False
+                dispensing = True
 
-        elif (stopfor == 0):
-            if (less and cl_A.value() >= (initial-2)):
+        elif stopfor == 0:
+            if less and cl_A.value() >= (initial - 2):
                 # if sensor value originally decreased, and value has now increased
                 # then dispense is complete
                 dispensing = False
@@ -68,7 +63,7 @@ while i < 2:
                 dispense_attempts = 0
                 print(str(count_dispensed) + ' pills dispensed')
                 stopfor = 20
-            elif (not less and cl_A.value() <= (initial+1) and cl_A.value() >= (initial-1)):
+            elif not less and (initial + 1) >= cl_A.value() >= (initial - 1):
                 # if sensor value originally increased, it may decrease below normal
                 # before returning to normal, so stop monitoring sensor value for 10 iterations
                 dispensing = False
@@ -78,18 +73,21 @@ while i < 2:
                 stopfor = 20
 
         direction_count += 1
-        if (stopfor != 0):
+        if stopfor != 0:
             stopfor -= 1
-        time.sleep(0.0005) # increase length of iteration
+        time.sleep(0.0005)  # increase length of iteration
     motor.stop()
-    i += 1
     time.sleep(1)
+
+
+dispense(motor_A, number_pills[0])
+dispense(motor_B, number_pills[1])
 
 time.sleep(0.5)
 initial = cl_B.value()
 
 while True:
     value = cl_B.value()
-    if value <= (initial-4) or value >= (initial+4):
+    if value <= (initial - 4) or value >= (initial + 4):
         print('Pills taken')
         break
--- dispensing/dispense_package.py
+++ dispensing/dispense_package.py
@@ -4,29 +4,29 @@ import ev3dev.ev3 as ev3
 import time
 
 cl = ev3.ColorSensor(ev3.INPUT_2)
-cl.mode='COL-COLOR'
+cl.mode = 'COL-COLOR'
 motor = ev3.Motor('outD')
 colour = sys.argv[1]
-colour_codes = {"black":1, "blue":2, "green":3, "yellow":4, "red":5, "white":6, "brown":7}
-stop_times = {1:0.1, 2:0.1, 3:0.04, 4:0.1, 5:0.1, 6:0.1, 7:0.2}
+colour_codes = {"black": 1, "blue": 2, "green": 3, "yellow": 4, "red": 5, "white": 6, "brown": 7}
+stop_times = {1: 0.1, 2: 0.1, 3: 0.04, 4: 0.1, 5: 0.1, 6: 0.1, 7: 0.2}
 skip = False
 timed_out = False
 try:
     colour_code = colour_codes[sys.argv[1]]
 except:
-    print('Invalid colour')
+    raise Exception('Invalid colour')
 
 start_time = time.time()
 print(start_time)
 
-if (cl.value() == colour_code):
+if cl.value() == colour_code:
     skip = True
 
 if not skip:
     motor.run_forever(speed_sp=150)
 
-    while ((cl.value() != colour_code) and (not timed_out)):
-        if ((time.time() - start_time) > 6.0):
+    while (cl.value() != colour_code) and (not timed_out):
+        if (time.time() - start_time) > 6.0:
             timed_out = True
             motor.stop()
         pass
@@ -34,18 +34,17 @@ if not skip:
     time.sleep(stop_times[colour_code])
     motor.stop()
 
-if (timed_out):
+if timed_out:
     print('Timed out')
 else:
     print('found ' + colour)
 
-cl.mode='COL-REFLECT'
+cl.mode = 'COL-REFLECT'
 
 time.sleep(1)
 
 initial = cl.value()
 
 while True:
-    if cl.value() <= (initial-2) or cl.value() >= (initial+2):
+    if cl.value() <= (initial - 2) or cl.value() >= (initial + 2):
         print('Package taken')
-
