6f02db28b1073a6b1d308636b9e464ebee8f6ca2
Jasper <jasper.prive@live.nl>
2018-04-03 11:35:18 +0100

    Added go loop

--- vision/pimain.py
+++ vision/pimain.py
@@ -102,77 +102,82 @@ client.loop_start()
 vs = PiVideoStream(resolution=(640, 480)).start()
 time.sleep(1)
 vs.camera.shutter_speed = 5000
-
-line_panic = 0
-
-# Find the first QR
 while True:
-    frame = vs.read()
 
-    qr_data = qrscanner.read_qr_whole(frame)
-    if len(qr_data) > 0:
-        handle_qr(qr_data)
-        break
-    else:
-        client.publish("movement", "start,-200,-200")
-        time.sleep(0.2)
-        client.publish("movement", "stop")
-        line_panic += 1
-        if line_panic > 10:
-            # Failed to find the initial QR - stop
-
-            r = requests.put(server_hostname + "/updatestatus", data={'status': 'error', 'details': 'cannot find first QR'})
-            break
-
-
-qr_delay = 0
-line_panic = 0
-
-# Loop until we find a QR
-print('SCANNING')
-while True:
-    frame = vs.read()
-
-    # We only want to read a QR code if we've not just seen one
-    if qr_delay >= 40:
-        qr_data = qrscanner.read_qr_whole(frame)
-        if len(qr_data) > 0:
-            client.publish("movement", "stop")
-            print('QR FOUND')
-            print(qr_data)
-            handle_qr(qr_data)
-            qr_delay = 0
-            time.sleep(0.2)
-
-    qr_delay += 1
-
-    line_frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
-    line_contours, clean = linedetect.detect_line(line_frame, line_colour)
-    if (len(line_contours) > 0):
-        direction = linedetect.extract_direction_max(line_contours, np.shape(line_frame))
-        direction = (0.5 * direction + 0.5 * linedetect.extract_direction_average(clean, np.shape(line_frame)))
-        print(direction)
-        direction = direction
-        max_speed = 400
-        if direction > 0:
-            left = max_speed
-            right = max_speed - (max_speed * direction)
-        else:
-            right = max_speed
-            left = max_speed + (max_speed * (direction))
-        client.publish("movement", "start," + str(-left) + "," + str(-right))
-
+    if requests.get(server_hostname + "/go").text == "True":
         line_panic = 0
+
+        # Find the first QR
+        while True:
+            frame = vs.read()
+
+            qr_data = qrscanner.read_qr_whole(frame)
+            if len(qr_data) > 0:
+                handle_qr(qr_data)
+                break
+            else:
+                client.publish("movement", "start,-200,-200")
+                time.sleep(0.2)
+                client.publish("movement", "stop")
+                line_panic += 1
+                if line_panic > 10:
+                    # Failed to find the initial QR - stop
+
+                    r = requests.put(server_hostname + "/updatestatus", data={'status': 'error', 'details': 'cannot find first QR'})
+                    break
+
+
+        qr_delay = 0
+        line_panic = 0
+
+        # Loop until we find a QR
+        print('SCANNING')
+        while True:
+            frame = vs.read()
+
+            # We only want to read a QR code if we've not just seen one
+            if qr_delay >= 40:
+                qr_data = qrscanner.read_qr_whole(frame)
+                if len(qr_data) > 0:
+                    client.publish("movement", "stop")
+                    print('QR FOUND')
+                    print(qr_data)
+                    handle_qr(qr_data)
+                    qr_delay = 0
+                    time.sleep(0.2)
+
+            qr_delay += 1
+
+            line_frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
+            line_contours, clean = linedetect.detect_line(line_frame, line_colour)
+            if (len(line_contours) > 0):
+                direction = linedetect.extract_direction_max(line_contours, np.shape(line_frame))
+                direction = (0.5 * direction + 0.5 * linedetect.extract_direction_average(clean, np.shape(line_frame)))
+                print(direction)
+                direction = direction
+                max_speed = 400
+                if direction > 0:
+                    left = max_speed
+                    right = max_speed - (max_speed * direction)
+                else:
+                    right = max_speed
+                    left = max_speed + (max_speed * (direction))
+                client.publish("movement", "start," + str(-left) + "," + str(-right))
+
+                line_panic = 0
+            else:
+                # No line found
+                if line_panic > 30:
+                    # if we don't find a line for a long time - stop and notify server
+                    client.publish("movement", "stop")
+                    time.sleep(0.2)
+                    client.publish("movement", "stop")
+                    break
+                    r = requests.put(server_hostname + "/updatestatus", data={'status': 'error', 'details': 'lost line'})
+                line_panic += 1
+
     else:
-        # No line found
-        if line_panic > 30:
-            # if we don't find a line for a long time - stop and notify server
-            client.publish("movement", "stop")
-            time.sleep(0.2)
-            client.publish("movement", "stop")
-            break
-            r = requests.put(server_hostname + "/updatestatus", data={'status': 'error', 'details': 'lost line'})
-        line_panic += 1
+        time.sleep(5)
 
 
 # do a bit of cleanup
